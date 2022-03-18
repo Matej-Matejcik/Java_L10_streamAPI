0a22f1c5ede1b825a1535062ae8b1312c17e599e
Matej-Matejcik <Matej.Matejcik@ness.com>
2022-03-18 11:43:09 +0100

    added exercise 1 and 2

--- src/sk/ness/matejcik/main.java
+++ src/sk/ness/matejcik/main.java
@@ -1,9 +1,24 @@
 package sk.ness.matejcik;
 
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class main {
 
     public static void main(String [ ] args)
     {
+        System.out.println("1.\tUsing Java Stream API find out sum of all numbers between 1 and 10000 that are dividable by both 3 and 5 but not dividable by 7 ");
+        System.out.println(IntStream.range(1,10001)
+                .filter(X -> X % 3 == 0 && X % 5 == 0)
+                .filter(X -> X % 7 != 0)
+                .sum());
+
+        System.out.println();
+        System.out.println("2.\tUsing Java Stream API print first 100 even numbers that are not dividable by 8");
+        IntStream.range(1,101)
+                .filter(X -> X % 8 != 0)
+                .forEach(X -> System.out.print(X + ", "));
+
     }
 
 }
