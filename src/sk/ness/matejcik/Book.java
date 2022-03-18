769b01ece331bf5193469d9b209928cf5001eab2
Matej-Matejcik <Matej.Matejcik@ness.com>
2022-03-18 14:35:26 +0100

    Added book class

---
+++ src/sk/ness/matejcik/Book.java
@@ -0,0 +1,22 @@
+package sk.ness.matejcik;
+
+class Book {
+
+    public Book(String name, Integer price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    private String name;
+
+    private Integer price;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Integer getPrice() {
+        return this.price;
+    }
+
+}
